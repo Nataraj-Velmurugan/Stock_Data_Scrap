88f77bf25b4a533f059300cb236714c7207acb5d
NatarajVelmurugan <nataraj_@outlook.com>
2021-02-28 15:24:10 +0530

    added multiple stocks

--- index.js
+++ index.js
@@ -55,19 +55,24 @@ cron.schedule('*/30 * * * * *', async () => {
     console.log(new Date().toLocaleTimeString())
     console.log('running a task every 30 seconds');
     
-    let resp = await axios.get('http://nimblerest.lisuns.com:4531/GetLastQuoteArray/?accessKey=5033487b-e9c3-4355-ad66-3c6824c069bd&exchange=NSE&instrumentIdentifiers=RELIANCE');
+    let resp = await axios.get('http://nimblerest.lisuns.com:4531/GetLastQuoteArray/?accessKey=5033487b-e9c3-4355-ad66-3c6824c069bd&exchange=NSE&instrumentIdentifiers=RELIANCE+ITC');
     // console.log(resp.data);
 
     if(resp.data === 'Authentication request received. Try request data in next moment.') {
-       resp = await axios.get('http://nimblerest.lisuns.com:4531/GetLastQuoteArray/?accessKey=5033487b-e9c3-4355-ad66-3c6824c069bd&exchange=NSE&instrumentIdentifiers=RELIANCE');
+       resp = await axios.get('http://nimblerest.lisuns.com:4531/GetLastQuoteArray/?accessKey=5033487b-e9c3-4355-ad66-3c6824c069bd&exchange=NSE&instrumentIdentifiers=RELIANCE+ITC');
     }
 
     let responseArray = resp.data;
 
     let [relianceData] = responseArray.filter(obj=>obj.INSTRUMENTIDENTIFIER==='RELIANCE');
+    let [itcData] = responseArray.filter(obj=>obj.INSTRUMENTIDENTIFIER==='ITC');
 
-    let result = await connection.awaitQuery(`INSERT INTO RELIANCE (LOW, HIGH) 
-                                              VALUES ('${relianceData.LOW}', '${relianceData.HIGH}')`);
+
+    let result = await connection.awaitQuery(`INSERT INTO STOCK_TABLE (STOCK_NAME, AVERAGE_TRADED_PRICE, BUY_PRICE,BUY_QTY, STOCK_CLOSE, HIGH, LAST_TRADE_PRICE, LAST_TRADE_QTY, LOW, STOCK_OPEN, SELL_PRICE, SELL_QTY, TOTAL_QTY_TRADED, PRICE_CHANGE, PRICE_CHANGE_PERCENTAGE)
+                                            VALUES 
+                                            ('${relianceData.INSTRUMENTIDENTIFIER}', '${relianceData.AVERAGETRADEDPRICE}', '${relianceData.BUYPRICE}','${relianceData.BUYQTY}','${relianceData.CLOSE}','${relianceData.HIGH}','${relianceData.LASTTRADEPRICE}', '${relianceData.LASTTRADEQTY}', '${relianceData.LOW}','${relianceData.OPEN}','${relianceData.SELLPRICE}','${relianceData.SELLQTY}','${relianceData.TOTALQTYTRADED}','${relianceData.PRICECHANGE}','${relianceData.PRICECHANGEPERCENTAGE}'),
+                                            ('${itcData.INSTRUMENTIDENTIFIER}', '${itcData.AVERAGETRADEDPRICE}', '${itcData.BUYPRICE}','${itcData.BUYQTY}','${itcData.CLOSE}','${itcData.HIGH}','${itcData.LASTTRADEPRICE}', '${itcData.LASTTRADEQTY}', '${itcData.LOW}','${itcData.OPEN}','${itcData.SELLPRICE}','${itcData.SELLQTY}','${itcData.TOTALQTYTRADED}','${itcData.PRICECHANGE}','${itcData.PRICECHANGEPERCENTAGE}')
+                                            `);
     // console.log(result);
 
 });
